b538021efce25443884469a4c0abeb8073dc1a01
Uwe Stuehler <ustuehler@growit.io>
2017-11-04 21:19:06 +0100

    wip

--- src/plugins/github-pages/syncer.js
+++ src/plugins/github-pages/syncer.js
@@ -1,6 +1,16 @@
 /*!
 
 title: $:/plugins/ustuehler/github-pages/syncer.js
-module-type: library
+module-type: utils
 
 !*/
+(function() {
+'use strict';
+
+var enabled = false;
+
+exports.setGithubPagesSync = function(enabled) {
+  this.enabled = enabled;
+};
+
+})();
