b0460a8c7aed6d26439621b857b9d6ff9ef8cb5c
Uwe Stuehler <ustuehler@growit.io>
2017-11-06 00:42:06 +0100

    wip

--- src/plugins/github-api/widgets/github-user-keys.js
+++ src/plugins/github-api/widgets/github-user-keys.js
@@ -38,9 +38,15 @@ GitHubUserKeysWidget.prototype.render = function(parent,nextSibling) {
 
   // Fetch and render the user keys using the supplied template
   $tw.utils.github.getUserKeys(this.user).then(function(keys) {
+		var domNode = self.domNode;
+
+		while (domNode.firstChild) {
+			domNode.removeChild(domNode.firstChild);
+		}
+
+		self.renderUserKeys(domNode, null);
+
 		$tw.utils.showSnackbar('Retrieved ' + keys.length + ' keys for ' + self.user + '.');
-		console.log('Got user keys:');
-		console.log(keys);
   }).catch(function(err) {
 		$tw.utils.showSnackbar('Failed to retrieve SSH public keys for ' + self.user + '.');
 	});
