9b25cffd0fb5c57ea43cf3cacb3790ba47fa8df2
Uwe Stuehler <ustuehler@growit.io>
2017-11-06 00:28:17 +0100

    wip

--- src/plugins/github-api/utils/github.js
+++ src/plugins/github-api/utils/github.js
@@ -64,8 +64,6 @@ function getUserKeys(username) {
 		var u = getUser(username);
 
 		u._request('GET', u.__getScopedUrl('keys'), null, function(err, response) {
-			console.log('err: ' + err);
-			console.log('response: ' + response);
 			if (err) {
 				reject(err);
 			} else {
