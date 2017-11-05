b65c144d91e5c44bf637b61ade4dff1c0c38b765
Uwe Stuehler <ustuehler@growit.io>
2017-11-05 20:56:12 +0100

    wip

--- src/plugins/github-api/widgets/github-user-keys.js
+++ src/plugins/github-api/widgets/github-user-keys.js
@@ -36,9 +36,9 @@ GitHubUserKeysWidget.prototype.render = function(parent,nextSibling) {
 Compute the internal state of the widget
 */
 GitHubUserKeysWidget.prototype.execute = function() {
-  this.tiddler = this.getAttribute("tiddler", this.getVariable("currentTiddler"));
-  this.field = this.getAttribute("field", "list");
-  this.filter = this.getAttribute("filter", "");
+  var defaultUser = this.getTemporarySetting("UserName", this.getSetting("username"));
+
+  this.user = this.getAttribute("user", defaultUser);
 
   // Compute the internal state of child widgets.
   this.makeChildWidgets();
@@ -56,74 +56,6 @@ GitHubUserKeysWidget.prototype.refresh = function(changedTiddlers) {
   }
 
   return this.refreshChildren(changedTiddlers);
-};
-
-/*
- * Invoke the action associated with this widget
- */
-GitHubUserKeysWidget.prototype.invokeAction = function(triggeringWidget,event) {
-  var username = this.getTemporarySetting("UserName", this.getSetting('username'));
-  var password = this.getTemporarySetting("Password", this.getSetting('password')); 
-  var title = this.tiddler;
-  var field = this.field;
-  var filter = this.filter;
-  var self = this;
-
-  console.log("GitHubUserKeysWidget.prototype.invokeAction");
-  console.log(self);
-
-  // TODO: move this code fragment to $tw.utils.github
-
-  // basic auth
-  var gh = new GitHub({
-    username: username,
-    password: password
-    /* also acceptable:
-    token: 'MY_OAUTH_TOKEN'
-    */
-  });
-
-  var me = gh.getUser(); // no user specified defaults to the user for whom credentials were provided
-
-  console.log('Listing GitHub repositories for authenticated user.');
-  me.listRepos(function(err, repos) {
-    if (err) {
-      $tw.utils.showSnackbar("Error from GitHub: " + err);
-      return;
-    }
-
-    console.log('Found ' + repos.length + ' repositories.');
-    console.log(repos);
-
-    var repoNames = [];
-
-    for (var i in repos) {
-      var fullName = repos[i].full_name;
-      repoNames.push(fullName);
-    }
-
-    var value = $tw.utils.stringifyList(repoNames);
-    var options = {};
-
-    // Reduce the list by a filter
-    if (filter.length > 0) {
-      repoNames = $tw.wiki.filterTiddlers(value + ' +' + filter);
-      value = $tw.utils.stringifyList(repoNames);
-      console.log('Reduced the list to ' + repoNames.length + ' repositories.');
-    }
-
-    console.log('Setting repository list to ' + value + '.');
-    $tw.wiki.setText(title, field, undefined, value, options);
-
-    var message = 'Found ' + repoNames.length + ' repositories.';
-    if (repoNames.length != repos.length) {
-      message = 'Found ' + repoNames.length + ' repositories matching the filter.';
-    }
-
-    $tw.utils.showSnackbar(message);
-  });
-
-  return true; // Action was invoked
 };
 
 // TODO: move getTemporarySetting and getSetting to $tw.utils.github
