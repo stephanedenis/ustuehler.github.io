5bb4490d60419d6778bdc558d32b403d3d5d9883
Uwe Stuehler <ustuehler@growit.io>
2017-11-06 13:41:15 +0100

    wip

--- src/plugins/oauth/widgets/action-githubsignin.js
+++ src/plugins/oauth/widgets/action-githubsignin.js
@@ -71,7 +71,7 @@ GitHubSignInWidget.prototype.invokeAction = function(triggeringWidget,event) {
    * Redirect to GitHub, let GitHub authenticate the user, and finally, let
    * GitHub redirect back to this TiddlyWiki.
    *
-   * Next, the githubauthcallback widget should be rendered somewhere on the
+   * Next, the github-auth widget should be rendered somewhere on the
    * page, which in turn invokes the callback() function in this module.
    */
   $tw.utils.oauth.requestToken();
@@ -81,10 +81,10 @@ GitHubSignInWidget.prototype.invokeAction = function(triggeringWidget,event) {
 };
 
 /*
- * Invoked by the githubauthcallback widget
+ * Invoked by the github-auth widget
  */
 function callback() {
-  console.log("githubauthcallback called back! \\o/");
+  console.log("github-auth called back! \\o/");
 
   // TODO: Set $:/status/OAuth/UserName et al
 }
