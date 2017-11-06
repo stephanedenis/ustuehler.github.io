1fd4f1057c3a8e4873ac651490fe619ce8c2ae13
Uwe Stuehler <ustuehler@growit.io>
2017-11-06 11:33:48 +0100

    wip

--- src/plugins/oauth/utils/oauth.js
+++ src/plugins/oauth/utils/oauth.js
@@ -13,12 +13,39 @@ Provides OAuth utility functions under $tw.utils.oauth
 /*jslint node: true, browser: true */
 /*global $tw: false */
 
+var github = null;
+
 function getClient() {
-  var OAuth = require("$:/plugins/ustuehler/oauth/oauth2-client.js");
-  var github = new OAuth.Provider({
-    id: 'github',
-    authorization_url: 'https://github.com/login/oauth/authorize'
-  });
+	if (github) {
+		return github;
+	}
+
+	let OAuth = require("$:/plugins/ustuehler/oauth/oauth2-client.js");
+
+	github = new OAuth.Provider({
+		id: 'github',
+		authorization_url: 'https://github.com/login/oauth/authorize'
+	});
+
+	return github;
+}
+
+function requestToken() {
+	// Create a new request
+	var request = new OAuth.Request({
+			client_id: 'my_client_id',  // required
+			redirect_uri: 'http://my.server.com/auth-answer'
+	});
+
+	// Give it to the provider
+	var uri = google.requestToken(request);
+
+	// Later we need to check if the response was expected
+	// so save the request
+	google.remember(request);
+
+	// Do the redirect
+	window.location.href = uri;
 
 }
 
