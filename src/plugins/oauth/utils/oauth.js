7c9681b484d98f5a64d060931b8a75e2ad267cb2
Uwe Stuehler <ustuehler@growit.io>
2017-11-06 21:01:01 +0100

    wip

--- src/plugins/oauth/utils/oauth.js
+++ src/plugins/oauth/utils/oauth.js
@@ -102,7 +102,7 @@ function openPopup(options) {
 	options.windowOptions = options.windowOptions || 'location=0,status=0,width=800,height=400';
 	options.callback = options.callback || function(){ window.location.reload(); };
 	var that = this;
-	log(options.path);
+	console.log(options.path);
 	that._oauthWindow = window.open(options.path, options.windowName, options.windowOptions);
 	that._oauthInterval = window.setInterval(function(){
 		if (that._oauthWindow.closed) {
