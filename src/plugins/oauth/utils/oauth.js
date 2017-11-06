d0d49d974345a5947d208f453b9acaaf266cc13d
Uwe Stuehler <ustuehler@growit.io>
2017-11-06 14:54:28 +0100

    wip

--- src/plugins/oauth/utils/oauth.js
+++ src/plugins/oauth/utils/oauth.js
@@ -38,7 +38,7 @@ function initialise(options) {
 
   // Update hardcoded defaults to current configuration
   defaultConfig.client_id = $tw.wiki.getTiddlerText('$:/config/OAuth/ClientID') || defaultConfig.client_id;
-  defaultConfig.redirect_uri = $tw.wiki.getTiddlerText('$:/config/OAuth/RedirectURI') || getRedirectURIFromWindow();
+  defaultConfig.redirect_uri = $tw.wiki.getTiddlerText('$:/config/OAuth/RedirectURI') || getRedirectURI();
 
   for (var attr in defaultConfig) {
     if (!config[attr] && defaultConfig.hasOwnProperty(attr)) {
