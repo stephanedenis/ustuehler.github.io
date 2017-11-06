0c2caf71b2b8067a0abb393ea8d40eede3cdd0f1
Uwe Stuehler <ustuehler@growit.io>
2017-11-06 15:10:44 +0100

    wip

--- src/plugins/oauth/utils/oauth.js
+++ src/plugins/oauth/utils/oauth.js
@@ -108,6 +108,21 @@ function getUserName() {
   return $tw.wiki.getTiddlerText('$:/status/OAuth/UserName');;
 }
 
+// ref: https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+var urlParams;
+
+(window.onpopstate = function () {
+	var match,
+			pl     = /\+/g,  // Regex for replacing addition symbol with a space
+			search = /([^&=]+)=?([^&]*)/g,
+			decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
+			query  = window.location.search.substring(1);
+
+	urlParams = {};
+	while (match = search.exec(query))
+		 urlParams[decode(match[1])] = decode(match[2]);
+})();
+
 exports.oauth = {
 	getProvider: getProvider, // only for inspection
   initialise: initialise,
