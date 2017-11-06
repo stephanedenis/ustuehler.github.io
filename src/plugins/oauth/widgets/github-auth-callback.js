173f1cf44a35e2927731c54741d46f9d8162b6a5
Uwe Stuehler <ustuehler@growit.io>
2017-11-06 12:13:57 +0100

    wip

--- src/plugins/oauth/widgets/github-auth-callback.js
+++ src/plugins/oauth/widgets/github-auth-callback.js
@@ -36,10 +36,6 @@ GitHubAuthCallback.prototype.render = function(parent,nextSibling) {
 Compute the internal state of the widget
 */
 GitHubAuthCallback.prototype.execute = function() {
-  this.tiddler = this.getAttribute("tiddler", this.getVariable("currentTiddler"));
-  this.field = this.getAttribute("field", "list");
-  this.filter = this.getAttribute("filter", "");
-
   // Compute the internal state of child widgets.
   this.makeChildWidgets();
 };
@@ -50,50 +46,8 @@ Selectively refreshes the widget if needed. Returns true if the widget or any of
 GitHubAuthCallback.prototype.refresh = function(changedTiddlers) {
   var changedAttributes = this.computeAttributes();
 
-  if (changedAttributes.tiddler || changedAttributes.field || changedAttributes.filter) {
-    this.refreshSelf();
-    return true;
-  }
-
   return this.refreshChildren(changedTiddlers);
 };
-
-/*
- * Invoke the action associated with this widget
- */
-GitHubAuthCallback.prototype.invokeAction = function(triggeringWidget,event) {
-  var self = this;
-
-  console.log("GitHubAuthCallback.prototype.invokeAction");
-  console.log(self);
-
-  /*
-   * $tw.utils.oauth.initialize({
-   *   ...
-   * });
-   */
-
-  /*
-   * Redirect to GitHub, let GitHub authenticate the user, and finally, let
-   * GitHub redirect back to this TiddlyWiki.
-   *
-   * Next, the githubauthcallback widget should be rendered somewhere on the
-   * page, which in turn invokes the callback() function in this module.
-   */
-  $tw.utils.oauth.requestToken();
-  // notreached
-
-  return true; // Action was invoked
-};
-
-/*
- * Invoked by the githubauthcallback widget
- */
-function callback() {
-  console.log("githubauthcallback called back! \\o/");
-
-  // TODO: Set $:/status/OAuth/UserName et al
-}
 
 exports["github-auth-callback"] = GitHubAuthCallback;
 
