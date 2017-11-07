0e55fa7d313a29ff54805d24d7aa7912cd52dc01
Uwe Stuehler <ustuehler@growit.io>
2017-11-07 02:38:50 +0100

    wip

--- src/plugins/firebaseui/widgets/firebaseui-auth-container.js
+++ src/plugins/firebaseui/widgets/firebaseui-auth-container.js
@@ -62,6 +62,45 @@ FirebaseUIAuthContainerWidget.prototype.render = function(parent,nextSibling) {
 };
 
 /*
+ * Auth
+ */
+FirebaseUIAuthContainerWidget.prototype.handleSignIn = function() {
+	firebase.auth().onAuthStateChanged(function(user) {
+		if (user) {
+			// User is signed in.
+			var displayName = user.displayName;
+			var email = user.email;
+			var emailVerified = user.emailVerified;
+			var photoURL = user.photoURL;
+			var uid = user.uid;
+			var phoneNumber = user.phoneNumber;
+			var providerData = user.providerData;
+			user.getIdToken().then(function(accessToken) {
+				document.getElementById('sign-in-status').textContent = 'Signed in';
+				document.getElementById('sign-in').textContent = 'Sign out';
+				document.getElementById('account-details').textContent = JSON.stringify({
+					displayName: displayName,
+					email: email,
+					emailVerified: emailVerified,
+					phoneNumber: phoneNumber,
+					photoURL: photoURL,
+					uid: uid,
+					accessToken: accessToken,
+					providerData: providerData
+				}, null, '  ');
+			});
+		} else {
+			// User is signed out.
+			document.getElementById('sign-in-status').textContent = 'Signed out';
+			document.getElementById('sign-in').textContent = 'Sign in';
+			document.getElementById('account-details').textContent = 'null';
+		}
+	}, function(error) {
+		console.log(error);
+	});
+}
+
+/*
 Compute the internal state of the widget
 */
 FirebaseUIAuthContainerWidget.prototype.execute = function() {
