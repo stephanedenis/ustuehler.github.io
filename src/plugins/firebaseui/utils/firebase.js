06ff55e63600540c7f29c4bc545d38703e6a3842
Uwe Stuehler <ustuehler@growit.io>
2017-11-07 21:59:15 +0100

    wip

--- src/plugins/firebaseui/utils/firebase.js
+++ src/plugins/firebaseui/utils/firebase.js
@@ -4,7 +4,8 @@ type: application/javascript
 module-type: utils
 caption: firebase
 
-Provides utility functions under $tw.utils.firebase
+Provides utility functions under $tw.utils.firebase, handles the initialisation
+of Firebase, and dispatches Firebase-related events such as auth state changes.
 
 This module is only available if the window variable is defined, because it
 doesn't have any useful functionality, otherwise.
