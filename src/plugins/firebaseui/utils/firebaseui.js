1c97fe846437a82bee1d26b88ae6e57b9b114159
Uwe Stuehler <ustuehler@growit.io>
2017-11-07 03:32:54 +0100

    wip

--- src/plugins/firebaseui/utils/firebaseui.js
+++ src/plugins/firebaseui/utils/firebaseui.js
@@ -277,6 +277,47 @@ function initialise(options) {
   });
 }
 
+function registerAuthStateListener() {
+	firebase.auth().onAuthStateChanged(function(user) {
+		if (user) {
+			// User is signed in.
+			var displayName = user.displayName;
+			var email = user.email;
+			var emailVerified = user.emailVerified;
+			var photoURL = user.photoURL;
+			var uid = user.uid;
+			var phoneNumber = user.phoneNumber;
+			var providerData = user.providerData;
+			user.getIdToken().then(function(accessToken) {
+        console.log('User is signed out.')
+        console.log('Access token: ' + accessToken);
+        /*
+				document.getElementById('sign-in-status').textContent = 'Signed in';
+				document.getElementById('sign-in').textContent = 'Sign out';
+				document.getElementById('account-details').textContent = JSON.stringify({
+					displayName: displayName,
+					email: email,
+					emailVerified: emailVerified,
+					phoneNumber: phoneNumber,
+					photoURL: photoURL,
+					uid: uid,
+					accessToken: accessToken,
+					providerData: providerData
+				}, null, '  ');
+        */
+			});
+		} else {
+      console.log('User is signed out.')
+			//document.getElementById('sign-in-status').textContent = 'Signed out';
+			//document.getElementById('sign-in').textContent = 'Sign in';
+			//document.getElementById('account-details').textContent = 'null';
+		}
+	}, function(error) {
+    console.log('Error in signin flow.')
+		console.log(error);
+	});
+}
+
 exports.firebaseui = {
   initialise: initialise,
 
