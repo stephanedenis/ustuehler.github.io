8f8e03b7530e38e35d8ca93fc0dfcbf782c8c52e
Uwe Stuehler <ustuehler@growit.io>
2017-11-07 21:53:21 +0100

    wip

--- src/plugins/firebaseui/utils/firebase.js
+++ src/plugins/firebaseui/utils/firebase.js
@@ -5,7 +5,8 @@ module-type: utils
 caption: firebase
 
 Provides utility functions under $tw.utils.firebase. This module is only
-available if the window variable is defined.
+available if the window variable is defined, because it doesn't have any
+useful functionality otherwise.
 
 \*/
 (function (global) { if (typeof window !== 'undefined') {
