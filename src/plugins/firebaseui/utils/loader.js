46f39525aa33eb661cdbb74e6e8290c38530cf4b
Uwe Stuehler <ustuehler@growit.io>
2017-11-07 22:19:17 +0100

    wip

---
+++ src/plugins/firebaseui/utils/loader.js
@@ -0,0 +1,35 @@
+/*\
+title: $:/plugins/ustuehler/firebaseui/utils/firebase.js
+type: application/javascript
+module-type: utils
+caption: firebase
+
+Provides utility functions under $tw.utils.firebase, handles the initialisation
+of Firebase, and dispatches Firebase-related events such as auth state changes.
+
+This module is only available if the window variable is defined, because it
+doesn't serve any useful purpose, otherwise.
+
+\*/
+(function (global) { if (typeof window !== 'undefined') {
+
+"use strict";
+/*jslint node: true, browser: true */
+/*global $tw: false */
+
+// Current status of the Firebase subsystem in this plugin
+var status = {
+  ok: true,
+  ready: false
+};
+
+exports.firebase = {
+  getStatus: function() { return status; },
+  initialise: function() {
+    return new Promise(function(resolve, reject) {
+      reject('TODO: how can we detect when firebase has finished loading asynchronously?');
+    });
+  }
+};
+
+}})(this);
