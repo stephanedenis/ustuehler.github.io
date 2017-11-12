979a674c97f79de199981b1b0f333b585789991a
Uwe Stuehler <ustuehler@growit.io>
2017-11-12 17:13:43 +0100

    wip

--- src/plugins/firebase/lib/firebaseui.js
+++ src/plugins/firebase/lib/firebaseui.js
@@ -195,7 +195,7 @@ FirebaseUI component
         //firebase.auth.GoogleAuthProvider.PROVIDER_ID,
         //firebase.auth.FacebookAuthProvider.PROVIDER_ID,
         //firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-        this.firebase_auth.GithubAuthProvider.PROVIDER_ID
+        this.firebase.auth.GithubAuthProvider.PROVIDER_ID
         //firebase.auth.EmailAuthProvider.PROVIDER_ID,
         //firebase.auth.PhoneAuthProvider.PROVIDER_ID
       ],
