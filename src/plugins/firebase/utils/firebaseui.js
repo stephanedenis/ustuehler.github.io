fcb15429b6fa1eba70490d4a5934de8635c91b92
Uwe Stuehler <ustuehler@growit.io>
2017-11-12 14:06:33 +0100

    wip

---
+++ src/plugins/firebase/utils/firebaseui.js
@@ -0,0 +1,23 @@
+/*\
+title: $:/plugins/ustuehler/firebase/utils/firebase.js
+type: application/javascript
+module-type: utils
+caption: firebase
+
+Registers Firebase plugin functions under $tw.utils.firebase, mainly to make
+them easily accessible for debugging in the browser console
+
+\*/
+(function () {
+  exports.assign = function (to, from) {
+    for (var p in from) {
+      if (from.hasOwnProperty(p)) {
+        to[p] = from[p]
+      }
+    }
+  }
+
+  exports.firebase = function () {
+    return require('$:/plugins/ustuehler/firebase/index.js').firebase
+  }
+})()
