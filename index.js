1a05ce706bd91f05050a44fe40e778647bdd6613
Uwe Stuehler <ustuehler@growit.io>
2017-11-12 16:48:19 +0100

    wip

--- index.js
+++ index.js
@@ -4,10 +4,10 @@
 This is invoked as a shell script by NPM when the `tiddlywiki` command is typed
 */
 
-var $tw = require("tiddlywiki/boot/boot.js").TiddlyWiki();
+var $tw = require('tiddlywiki/boot/boot.js').TiddlyWiki()
 
 // Pass the command line arguments to the boot kernel
-$tw.boot.argv = ["editions/hack", "--server", "8081"];
+$tw.boot.argv = ['editions/hack', '--server', '8081']
 
 // Boot the TW5 app
-$tw.boot.boot();
+$tw.boot.boot()
